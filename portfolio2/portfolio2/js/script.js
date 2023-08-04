dee9c1b41253f1e51a926f90d099056b1c68bb20
Ashish Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-08-04 12:47:37 +0530

    Add files via upload

--- portfolio2/portfolio2/js/script.js
+++ portfolio2/portfolio2/js/script.js
@@ -47,29 +47,6 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
 });
 
 
@@ -78,26 +55,3 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
-
-
-//  $(document).ready(function(){
-// 	 $(window).scroll(function(){
-// 		 if(this.scrollY > 20){
-// 			 $('.navbar').addClass("sticky");
-// 		 }else{
-// 			 $('.navbar').removeClass("sticky");
-// 		 }
-// 	 });
-	 
-// 	 //toggle menu / navbar scripts
-// 	 $('.menu-btn').click(function(){
-// 		 $('.navbar .menu').toggleClass("active");
-// 		 $('.menu-btn i').toggleClass("active");
-// 	 });
-//  });
